575819ba04b2e7e88e6e52eb1c9818f4b74b5406
chhimsokrith <sokrithchhim@gmail.com>
2025-01-12 18:42:28 +0700

    update_loading_logo

--- components/LoadingPage.tsx
+++ components/LoadingPage.tsx
@@ -12,22 +12,22 @@ const LoadingPage = () => {
                     
 
                     {/* Enhanced Middle Glow */}
-                    <div className="absolute w-28 h-28 rounded-full bg-gradient-to-r from-blue-500 to-purple-500 opacity-30 animate-ping"></div>
+                    <div className="absolute w-32 h-32 rounded-full bg-gradient-to-r from-blue-500 to-purple-500 opacity-30 animate-ping"></div>
 
                     {/* Spinner */}
-                    <div className="relative w-20 h-20 border-t-4 border-b-4 border-purple-500 border-opacity-75 rounded-full animate-spin">
+                    <div className="relative w-28 h-28 border-t-4 border-b-4 border-purple-500 border-opacity-75 rounded-full animate-spin">
                         {/* Rotating Gradient Effect */}
                         <div className="absolute inset-0 w-full h-full bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-full opacity-30 blur-sm"></div>
                     </div>
 
                     {/* Inner Glow */}
-                    <div className="absolute w-20 h-20 bg-gradient-to-r from-pink-400 to-purple-400 rounded-full opacity-50"></div>
+                    <div className="absolute w-28 h-28 bg-gradient-to-r from-pink-400 to-purple-400 rounded-full opacity-50"></div>
 
                     {/* Logo */}
                     <img
-                        src="https://res.cloudinary.com/dfxqagrkk/image/upload/v1736680213/favicon_hsdwji.png"
+                        src="https://res.cloudinary.com/dfxqagrkk/image/upload/v1736682037/ICG_200px-01_rn1xpj.png"
                         alt="Logo"
-                        className="absolute w-12 h-12 z-10"
+                        className="absolute w-20 h-20 z-10"
                     />
                 </div>
 
