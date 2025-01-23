8f7f6fbbf3a18d48e489313391fe55a16a397a34
chhimsokrith <sokrithchhim@gmail.com>
2025-01-23 20:02:40 +0700

    dfsa

--- app/[locale]/myaccount/referral/InviteFriendButton.tsx
+++ app/[locale]/myaccount/referral/InviteFriendButton.tsx
@@ -45,7 +45,7 @@ const InviteFriendButton = () => {
               >
                 {option.isHeroIcon ? (
                   // Render Heroicons
-                  <option.icon className="w-5 h-5 mr-2" />
+                  React.createElement(option.icon, { className: 'w-5 h-5 mr-2' })
                 ) : (
                   // Render FontAwesome icons
                   <FontAwesomeIcon icon={option.icon} className="w-5 h-5 mr-2" />
